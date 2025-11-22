5342d630c722d0b652942da1190cf95462daeac1
mkolen-boop <mkolendzyan5450@gmail.com>
2025-11-22 14:50:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const puppeteer = require("puppeteer-core");
-
+const adsRoute = require("./ads");
 const app = express();
+
 app.use(express.json());
 
 app.use("/ads", adsRoute);
