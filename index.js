fe52131d3f3bb962c6913432ebf340148f429a84
mkolen-boop <mkolendzyan5450@gmail.com>
2025-11-22 11:19:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import express from "express";
-import puppeteer from "puppeteer-core";
+const express = require("express");
+const puppeteer = require("puppeteer-core");
 
 const app = express();
 app.use(express.json());
